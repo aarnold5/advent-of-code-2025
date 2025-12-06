da9f75759ab345777eea9bfff9c82f99ec48ac7b
aarnold5 <alaina.j.arnold.22@dartmouth.edu>
2025-12-05 20:19:59 -0600

    day five part one

---
+++ day 5/day-five-part-one.py
@@ -0,0 +1,28 @@
+def solve_day_four_part_one():
+    f = open("inputs/day-five-input.txt", "r")
+
+    numFresh = 0
+    freshIdRanges = []
+    readingRanges = True
+
+    for line in f:
+        if (line == "\n"):
+            readingRanges = False
+            continue
+        
+        if readingRanges:
+            line = line.strip()
+            [firstNum, secondNum] = line.split("-")
+            freshIdRanges.append((int(firstNum), int(secondNum)))
+        else:
+            id = int(line.strip())
+            for idRange in freshIdRanges:
+                if idRange[0] <= id <= idRange[1]:
+                    numFresh += 1
+                    break
+            
+    f.close()
+
+    return numFresh
+
+print(solve_day_four_part_one())
